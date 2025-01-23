6376fd0f9db4063be24ec86ff39c791bc47cbaea
samias9 <samiacarchaf2003@gmail.com>
2025-01-23 13:53:10 -0500

    Step 4-2

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,10 +37,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(), //Remplit tout l'écran.
                     color = MaterialTheme.colorScheme.background //Définit la couleur de fond basée sur le thème
                 ) {
-                    GreetingText(
+                    GreetingImage(
                         message = "Happy Birthday Sam!",
                         from = "From Samia" ,
-                        modifier = Modifier.padding(8.dp)
                     )
                 }
             }
@@ -71,17 +71,23 @@ fun GreetingText(message: String,from: String,modifier: Modifier = Modifier){
 @Composable
 fun GreetingImage(message: String, from:String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty)
-    Image(
-        painter = image,
-        contentDescription = null
-    )
+    Box (modifier){
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        GreetingText(message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        //GreetingText(message = "Happy Birthday Sam!", from = "From Samia")
         GreetingImage(
             message = "Happy Birthday Sam!",
             from = "From Samia"
